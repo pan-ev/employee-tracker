69d25d1dbc742f7201b93ca1e0a42234ac60ca80
epan <epan616@gmail.com>
2023-04-29 01:14:58 -0400

    Add addEmployee functionality

--- db/queries.sql
+++ db/queries.sql
@@ -35,6 +35,4 @@ INSERT INTO department (id, name)
 VALUES 
     (5, "Service");
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES 
-    (1, "John", "Doe", 1, null);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, "John", "Doe", 1, null);
--- index.js
+++ index.js
@@ -3,20 +3,27 @@ const inquirer = require("inquirer");
 const queries = require("./lib/queries.js");
 const cTable = require("console.table");
 
-const db = mysql.createConnection(
-  {
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
-    database: "employee_db",
-  },
-);
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "employee_db",
+});
 
 class Role {
   constructor(title, salary, department) {
     this.title = title;
     this.salary = salary;
     this.department = department;
+  }
+}
+
+class Employee {
+  constructor(firstName, lastName, role, manager) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.role = role;
+    this.manager = manager;
   }
 }
 
@@ -73,37 +80,99 @@ function init() {
             },
           ])
           .then((input) => {
-            const newRole = new Role(input.job_title, input.salary,);
+            const newRole = new Role(input.job_title, input.salary);
             db.query("SELECT * FROM department", function (err, result) {
               var departmentList = result.map(({ name, id }) => ({
                 name: name,
                 value: id,
               }));
 
-              inquirer.prompt([
-                {
-                  type: "list",
-                  message: "Select the role's department",
-                  name: "depChoice",
-                  choices: departmentList,
-                },
-              ])
-              .then((data) => {
-                newRole.department = data.depChoice;
-                queries.addRole(db,newRole);
-              });
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    message: "Select the role's department",
+                    name: "depChoice",
+                    choices: departmentList,
+                  },
+                ])
+                .then((data) => {
+                  newRole.department = data.depChoice;
+                  queries.addRole(db, newRole);
+                });
             });
           });
       } else if (data.action === "Add an Employee") {
-        queries.addEmployee(db);
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "first_name",
+              message: "Please enter the employee's first name",
+            },
+            {
+              type: "input",
+              name: "last_name",
+              message: "Please enter the employee's last name",
+            },
+          ])
+          .then((input) => {
+            const newEmployee = new Employee(
+              input.first_name,
+              input.last_name,
+              null,
+              null
+            );
+            db.query("SELECT * FROM role", function (err, result) {
+              var roleList = result.map(({ title, id }) => ({
+                name: title,
+                value: id,
+              }));
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    message: "Select the employee's role",
+                    name: "roleChoice",
+                    choices: roleList,
+                  },
+                ])
+                .then((roleInput) => {
+                  newEmployee.role = roleInput.roleChoice;
+                  db.query("SELECT * FROM employee", function (err, result) {
+                    var managerList = result.map(
+                      ({ first_name, last_name, id }) => ({
+                        name: `${first_name} ${last_name}`,
+                        value: id,
+                      })
+                    );
+                    managerList.push({ name: "Not Applicable", value: null });
+                    inquirer
+                      .prompt([
+                        {
+                          type: "list",
+                          message: "Select the employee's manager",
+                          name: "managerChoice",
+                          choices: managerList,
+                        },
+                      ])
+                      .then((managerInput) => {
+                        newEmployee.manager = managerInput.managerChoice;
+                        queries.addEmployee(db, newEmployee);
+                      });
+                  });
+                });
+            });
+          });
       } else if (data.action === "Update an Employee Role") {
         queries.updateEmployeeRole(db);
       } else if (data.action === "Quit") {
         process.exit();
       }
+    })
+    .then(() => {
+      init();
     });
 }
-
-var departmentList;
 
 init();
